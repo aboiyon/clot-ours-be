57b1d33abf34294e26744560606b06bb4dd00de5
kiprotich <kiprotichmichael570@gmail.com>
2023-09-06 16:08:51 +0300

    refactor: add return type to get method to check spark set up if success

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,11 @@
+import static spark.Spark.get;
+
 public class App {
+    public static void main(String[] args) {
+        get("/hello", (request, response) -> {
+            response.type("text/html");
+            return "<html><body><h1>Hello Friend!</h1></body></html>";
+
+        });
+    }
 }
