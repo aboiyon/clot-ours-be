0e1c7efeb61725678497d27791b933680bf750f2
kiprotich <kiprotichmichael570@gmail.com>
2023-09-07 17:01:06 +0300

    refactor: add tests to remaining Beverages class methods

--- src/test/java/models/BeveragesTest.java
+++ src/test/java/models/BeveragesTest.java
@@ -13,8 +13,37 @@ public class BeveragesTest {
     }
 
     @Test
-    public void input_correctly() {
-        Beverages beverages = new Beverages("engineering","My fav", "", 20);
-        assertEquals("engineering",beverages.getBeverageName());
+    public void testSetBeverageId () {
+        Beverages beverages = new Beverages("","","", 0);
+        beverages.setId(0);
+        assertEquals(0, beverages.getId());
+    }
+
+    @Test
+    public void beverage_name_set_correctly() {
+        Beverages beverages = new Beverages("beer","My fav", "", 20);
+        beverages.setBeverageName("beer");
+        assertEquals("beer",beverages.getBeverageName());
+    }
+
+    @Test
+    public void beverage_description_set_correctly() {
+        Beverages beverages = new Beverages("","", "", 0);
+        beverages.setBeverageDescription("Italian brewed");
+        assertEquals("Italian brewed",beverages.getBeverageDescription());
+    }
+
+    @Test
+    public void testSetBeverageImageUrl () {
+        Beverages beverages = new Beverages("", "","",0);
+        beverages.setImageUrl("www.beer.com");
+        assertEquals("www.beer.com", beverages.getImageUrl());
+    }
+
+    @Test
+    public void testSetBeveragePrice () {
+        Beverages beverages = new Beverages("","","", 0);
+        beverages.setPrice(200);
+        assertEquals(200, beverages.getPrice());
     }
 }
