4652169887faae9e559f7de4a57e9333a2d35ca8
kiprotich <kiprotichmichael570@gmail.com>
2023-09-12 18:36:28 +0300

    add route to create a new beverage item and retrieve it

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,7 @@
 import com.google.gson.Gson;
+import models.Beverages;
+import models.Clothes;
+import org.sql2o.Sql2o;
 import sql2o.Sql2oBeverages;
 import sql2o.Sql2oClothes;
 import sql2o.Sql2oLinks;
@@ -8,19 +11,50 @@ import sql2o.Sql2oTours;
 import java.sql.Connection;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class App {
     public static void main(String[] args) {
-        get("/hello", (request, response) -> {
-            response.type("text/html");
-            return "<html><body><h1>Hello Friend!</h1></body></html>";
+//        get("/hello", (request, response) -> {
+//            response.type("text/html");
+//            return "<html><body><h1>Hello Friend!</h1></body></html>";
+//
+//        });
+        Sql2oBeverages beverages;
+        Sql2oClothes clothes;
+        Sql2oLinks links;
+        Sql2oTours tours;
+        Connection conn;
+        Gson gson = new Gson();
 
+        String connectionString = "";
+        Sql2o  sql2o = new Sql2o(connectionString, "cheruiyot", "");
+
+        beverages = new Sql2oBeverages();
+        clothes = new Sql2oClothes();
+        links = new Sql2oLinks();
+        tours = new Sql2oTours();
+
+        post("/beverage/new", "application/json", (req, res) -> {
+            Beverages beverage = gson.fromJson(req.body(), Beverages.class);
+            beverages.create(beverage);
+            res.status(201);
+            res.type("application/json");
+            return gson.toJson(beverages);
         });
+
+        get("/beverage", "application/json", (req, res) -> {
+            res.type("application/json");
+            return gson.toJson(beverages.findAll());
+        });
+
+//        post("/clothe/new", "application/json", (req, res) -> {
+//            Clothes clothe = gson.fromJson(req.body(), Clothes.class);
+//            clothes.create(clothe);
+//            res.status(201);
+//            res.type("application/json");
+//            return gson.toJson(clothes);
+//        });
     }
-    Sql2oBeverages departments;
-    Sql2oClothes clothes;
-    Sql2oLinks links;
-    Sql2oTours tours;
-    Connection conn;
-    Gson gson = new Gson();
+
 }
