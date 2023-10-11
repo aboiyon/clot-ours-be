126fce6d3842fd56907c3d9c9e5e24f2c0db5839
kiprotich <kiprotichmichael570@gmail.com>
2023-10-11 17:48:37 +0300

    rename tour name and tour description properties to name and description respectively to match product interface properties in the frontend

--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -4,12 +4,20 @@ CREATE DATABASE utalii;
 \c utalii;
 
 CREATE TABLE IF NOT EXISTS tours (
-id SERIAL PRIMARY KEY,
-tourName VARCHAR,
-tourDescription VARCHAR,
-imageUrl VARCHAR,
-price int
+    id SERIAL PRIMARY KEY,
+    name VARCHAR,
+    description VARCHAR,
+    imageUrl VARCHAR,
+    imageId INT,
+    price int
 );
+
+CREATE TABLE IF NOT EXISTS images (
+    id SERIAL PRIMARY KEY,
+    tourId INT,
+    imageData bytea
+);
+
 
 CREATE TABLE IF NOT EXISTS reviews (
  id SERIAL PRIMARY KEY,
